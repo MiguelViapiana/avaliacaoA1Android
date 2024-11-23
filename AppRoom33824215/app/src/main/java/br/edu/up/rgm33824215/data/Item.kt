3214ab84a037c32b4f3d3fb26c9df834770fecce
MiguelViapiana <migvjung@gmail.com>
2024-11-23 14:46:42 -0300

    DAO adicionada

--- AppRoom33824215/app/src/main/java/br/edu/up/rgm33824215/data/Item.kt
+++ AppRoom33824215/app/src/main/java/br/edu/up/rgm33824215/data/Item.kt
@@ -1,6 +1,11 @@
 package br.edu.up.rgm33824215.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "items")
 class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
