b85021dea785d1b01eb4db5177fca7ee941f4e9b
MiguelViapiana <migvjung@gmail.com>
2024-11-23 16:12:31 -0300

    listagem de items adicionada

--- AppRoom33824215/app/src/main/java/br/edu/up/rgm33824215/ui/home/HomeScreen.kt
+++ AppRoom33824215/app/src/main/java/br/edu/up/rgm33824215/ui/home/HomeScreen.kt
@@ -24,6 +24,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -32,9 +34,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.edu.up.rgm33824215.InventoryTopAppBar
 import br.edu.up.rgm33824215.R
 import br.edu.up.rgm33824215.data.Item
+import br.edu.up.rgm33824215.ui.AppViewModelProvider
 import br.edu.up.rgm33824215.ui.item.formatedPrice
 import br.edu.up.rgm33824215.ui.navigation.NavigationDestination
 
@@ -51,9 +55,11 @@ object HomeDestination : NavigationDestination {
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     navigateToItemUpdate: (Int) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val homeUiState by viewModel.homeUiState.collectAsState()
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -78,7 +84,7 @@ fun HomeScreen(
         },
     ) { innerPadding ->
         HomeBody(
-            itemList = listOf(),
+            itemList = homeUiState.itemList,
             onItemClick = navigateToItemUpdate,
             modifier = modifier.fillMaxSize(),
             contentPadding = innerPadding,
